a47fde88fde8b99a65e30e35e75c20a828445200
Azami <avatarofslack@gmail.com>
2018-09-16 11:26:33 -0700

    WIP

---
+++ Ollivanders/src/net/pottercraft/Ollivanders2/Potion/CURE_FOR_BOILS.java
@@ -0,0 +1,22 @@
+package net.pottercraft.Ollivanders2.Potion;
+
+import net.pottercraft.Ollivanders2.Ollivanders2;
+
+/**
+ * The Cure for Boils (also known as simply Boil Cure) is a potion which cures boils, even those produced by the Pimple Jinx.
+ *
+ * @author Azami7
+ * @since 2.2.8
+ */
+public class CURE_FOR_BOILS extends O2Potion
+{
+   /**
+    * Constructor.
+    *
+    * @param plugin the callback to the MC plugin
+    */
+   public CURE_FOR_BOILS (Ollivanders2 plugin)
+   {
+      super(plugin);
+   }
+}
