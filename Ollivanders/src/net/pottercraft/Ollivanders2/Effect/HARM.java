0b25f1beec3367bea4b45ff176ab622beec2e00e
Kristin Amundsen <avatarofslack@gmail.com>
2019-01-06 00:24:30 -0800

    Divination merge (#260)

--- Ollivanders/src/net/pottercraft/Ollivanders2/Effect/HARM.java
+++ Ollivanders/src/net/pottercraft/Ollivanders2/Effect/HARM.java
@@ -13,19 +13,19 @@ import java.util.UUID;
  */
 public class HARM extends PotionEffectSuper
 {
-    int strength = 1;
+   int strength = 1;
 
-    public HARM (Ollivanders2 plugin, Integer duration, UUID pid)
-    {
-        super(plugin, duration, pid);
+   public HARM (Ollivanders2 plugin, Integer duration, UUID pid)
+   {
+      super(plugin, duration, pid);
 
-        effectType = O2EffectType.HARM;
-        potionEffectType = PotionEffectType.HARM;
-        informousText = legilimensText = "feels unwell";
+      effectType = O2EffectType.HARM;
+      potionEffectType = PotionEffectType.HARM;
+      informousText = legilimensText = "feels unwell";
 
-        divinationText.add("shall be struck by a terrible affliction");
-        divinationText.add("will come to harm");
-        divinationText.add("shall be cursed");
-        divinationText.add("will be develop a terrible illness");
-    }
+      divinationText.add("shall be struck by a terrible affliction");
+      divinationText.add("will come to harm");
+      divinationText.add("shall be cursed");
+      divinationText.add("will be develop a terrible illness");
+   }
 }
