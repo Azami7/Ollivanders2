06fce51546a887f6f58e07216f2764af8801d403
Azami <avatarofslack@gmail.com>
2019-03-03 15:53:27 -0800

    https://github.com/Azami7/Ollivanders2/issues/280
    * Completely rewrote dissendium to use 1.13 API and align better with HP canon

--- Ollivanders/src/net/pottercraft/Ollivanders2/Spell/DISSENDIUM.java
+++ Ollivanders/src/net/pottercraft/Ollivanders2/Spell/DISSENDIUM.java
@@ -2,27 +2,28 @@ package net.pottercraft.Ollivanders2.Spell;
 
 import net.pottercraft.Ollivanders2.Ollivanders2;
 import net.pottercraft.Ollivanders2.Ollivanders2API;
-import org.bukkit.block.BlockState;
+import net.pottercraft.Ollivanders2.StationarySpell.COLLOPORTUS;
+import net.pottercraft.Ollivanders2.StationarySpell.StationarySpellObj;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.TrapDoor;
 import org.bukkit.entity.Player;
-import org.bukkit.material.MaterialData;
-import org.bukkit.material.Openable;
-
-import net.pottercraft.Ollivanders2.StationarySpell.O2StationarySpellType;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Opens a trapdoor or door.
+ * Opens a trapdoor
  *
- * @author lownes
  * @author Azami7
  */
 public final class DISSENDIUM extends Charms
 {
-   private double lifeTime;
-   private boolean move;
+   private double maxOpenTime;
    private int openTime;
-   private boolean open;
+   private boolean isOpen;
+   private Block trapDoorBlock;
 
    /**
     * Default constructor for use in generating spell text.  Do not use to cast the spell.
@@ -55,51 +56,103 @@ public final class DISSENDIUM extends Charms
       spellType = O2SpellType.DISSENDIUM;
       setUsesModifier();
 
-      lifeTime = usesModifier * 16;
-      move = true;
-      openTime = 160;
-      open = true;
+      maxOpenTime = usesModifier * 20;
+
+      p.getLogger().info("dissendium max open time is " + maxOpenTime);
+
+      openTime = 0;
+      isOpen = false;
    }
 
+   /**
+    * Move the spell projectile until it hits a block, if that is a trapdoor, open it and keep it open until the max
+    * duration of the spell is reached.
+    */
+   @Override
    public void checkEffect ()
    {
-      if (move)
+      // continue until the spell opens a trapdoor, hits another block type and is killed, or projectile expires
+      if (!isOpen)
       {
          move();
-         if (Ollivanders2API.getStationarySpells().isInsideOf(O2StationarySpellType.COLLOPORTUS, location))
+
+         Block target = getBlock();
+         BlockData targetBlockData = target.getBlockData();
+
+         if (targetBlockData instanceof TrapDoor)
          {
-            kill();
-            return;
-         }
-         if (getBlock().getState().getData() instanceof Openable)
-         {
-            kill = false;
-            move = false;
-            open = ((Openable) getBlock().getState().getData()).isOpen();
+            openTrapDoor();
          }
       }
       else
       {
-         openTime--;
-         BlockState state = getBlock().getState();
-         Openable openable = (Openable) state.getData();
-         if (openTime > 0)
+         // count down the open time
+         openTime++;
+
+         p.getLogger().info("open time = " + openTime);
+
+         if (openTime >= maxOpenTime)
          {
-            openable.setOpen(!open);
-            state.setData((MaterialData) openable);
-            state.update();
-         }
-         else
-         {
-            openable.setOpen(open);
-            state.setData((MaterialData) openable);
-            state.update();
+            closeTrapDoor();
             kill();
          }
       }
-      if (lifeTicks > lifeTime)
+   }
+
+   /**
+    * Opens the trapdoor at target block
+    */
+   private void openTrapDoor ()
+   {
+      Block target = getBlock();
+
+      // check for colloportus spell locking this door
+      Location targetLocation = target.getLocation();
+      List<StationarySpellObj> spellsAtLocation = Ollivanders2API.getStationarySpells().getStationarySpellsAtLocation(targetLocation);
+
+      for (StationarySpellObj statSpell : spellsAtLocation)
       {
-         kill();
+         if (statSpell instanceof COLLOPORTUS)
+         {
+            p.getLogger().info("trapdoor in a colloportus");
+            kill();
+            return;
+         }
       }
+
+      BlockData targetBlockData = target.getBlockData();
+
+      // check to see if the trap door is already open
+      if (((TrapDoor) targetBlockData).isOpen())
+      {
+         p.getLogger().info("trapdoor already open");
+         kill();
+         return;
+      }
+
+      trapDoorBlock = target;
+      ((TrapDoor) targetBlockData).setOpen(true);
+      trapDoorBlock.setBlockData(targetBlockData);
+
+      isOpen = true;
+      kill = false;
+   }
+
+   /**
+    * Close the trap door
+    */
+   private void closeTrapDoor ()
+   {
+      if (!isOpen)
+      {
+         return;
+      }
+
+      // close the trap door
+      TrapDoor trapDoorData = (TrapDoor) trapDoorBlock.getBlockData();
+      trapDoorData.setOpen(false);
+      trapDoorBlock.setBlockData(trapDoorData);
+
+      isOpen = false;
    }
 }
