a96685f3ce26b9efc3a7d5d435136d895e8fb9eb
Kristin Amundsen <amundsen@amazon.com>
2020-05-09 17:03:03 -0700

    changes from feedback
    https://github.com/Azami7/Ollivanders2/pull/312

--- Ollivanders/src/net/pottercraft/ollivanders2/spell/HARMONIA_NECTERE_PASSUS.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/spell/HARMONIA_NECTERE_PASSUS.java
@@ -71,7 +71,7 @@ public final class HARMONIA_NECTERE_PASSUS extends O2Spell
       Block fromBlock = getTargetBlock();
       Material blockType = fromBlock.getType();
 
-      if (common.wallSigns.contains(blockType))
+      if (common.signs.contains(blockType))
       {
          // determine the location of the other vanishing cabinet
          Location toLoc = getSignLocation(fromBlock);
@@ -85,7 +85,7 @@ public final class HARMONIA_NECTERE_PASSUS extends O2Spell
 
          Block toBlock = toLoc.getBlock();
          Material toBlockType = toBlock.getType();
-         if (!common.wallSigns.contains(toBlockType))
+         if (!common.signs.contains(toBlockType))
          {
             if (Ollivanders2.debug)
                p.getLogger().info("Block at toLoc is not a sign block");
