07185aa37ad4899a554c6435fcb6b9cee8a560d0
Azami7 <kamundse@gmail.com>
2021-06-17 20:18:18 -0700

    Fix zone config

--- Ollivanders/src/net/pottercraft/ollivanders2/Ollivanders2.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/Ollivanders2.java
@@ -1823,101 +1823,104 @@ public class Ollivanders2 extends JavaPlugin
    }
 
    /**
-    * Determine if this spell can exist here
+    * Determine if this spell can exist here based on zone config
     *
-    * @param loc   the location of the spell
+    * @param loc   the location the spell was cast
     * @param spell the spell to check
     * @return true if the spell can exist, false otherwise
     */
    public boolean isSpellTypeAllowed(@NotNull Location loc, @NotNull O2SpellType spell)
    {
+      if (zoneConfig == null)
+      {
+         getLogger().info("isSpellTypeAllowed: no zone config");
+         return true;
+      }
+
       boolean cast = true;
       double x = loc.getX();
       double y = loc.getY();
       double z = loc.getZ();
 
-      if (zoneConfig != null)
+      for (String zone : zoneConfig.getKeys(false))
       {
-         for (String zone : zoneConfig.getKeys(false))
+         String prefix = zone + ".";
+         String type = zoneConfig.getString(prefix + "type");
+         String world = zoneConfig.getString(prefix + "world");
+         String areaString = zoneConfig.getString(prefix + "area");
+
+         if (type == null || world == null || areaString == null)
+            return true;
+
+         boolean allAllowed = false;
+         boolean allDisallowed = false;
+         List<O2SpellType> allowedSpells = new ArrayList<>();
+         for (String spellString : zoneConfig.getStringList(prefix + "allowed-spells"))
          {
-            String prefix = zone + ".";
-            String type = zoneConfig.getString(prefix + "type");
-            String world = zoneConfig.getString(prefix + "world");
-            String areaString = zoneConfig.getString(prefix + "area");
-
-            if (type == null || world == null || areaString == null)
-               return true;
-
-            boolean allAllowed = false;
-            boolean allDisallowed = false;
-            List<O2SpellType> allowedSpells = new ArrayList<>();
-            for (String spellString : zoneConfig.getStringList(prefix + "allowed-spells"))
+            if (spellString.equalsIgnoreCase("ALL"))
             {
-               if (spellString.equalsIgnoreCase("ALL"))
+               allAllowed = true;
+            } else
+            {
+               allowedSpells.add(Ollivanders2API.getSpells(this).getSpellTypeByName(spellString));
+            }
+         }
+         List<O2SpellType> disallowedSpells = new ArrayList<>();
+         for (String spellString : zoneConfig.getStringList(prefix + "disallowed-spells"))
+         {
+            if (spellString.equalsIgnoreCase("ALL"))
+            {
+               allDisallowed = true;
+            }
+            else
+            {
+               disallowedSpells.add(Ollivanders2API.getSpells(this).getSpellTypeByName(spellString));
+            }
+         }
+         if (type.equalsIgnoreCase("World"))
+         {
+            if (loc.getWorld() != null && loc.getWorld().getName().equals(world))
+            {
+               if (allowedSpells.contains(spell) || allAllowed)
                {
-                  allAllowed = true;
-               } else
+                  return true;
+               }
+               if (disallowedSpells.contains(spell) || allDisallowed)
                {
-                  allowedSpells.add(Ollivanders2API.getSpells(this).getSpellTypeByName(spellString));
+                  cast = false;
                }
             }
-            List<O2SpellType> disallowedSpells = new ArrayList<>();
-            for (String spellString : zoneConfig.getStringList(prefix + "disallowed-spells"))
+         }
+         if (type.equalsIgnoreCase("Cuboid"))
+         {
+            String[] areaStringList = areaString.split(" ");
+            List<Integer> area = new ArrayList<>();
+            for (String a : areaStringList)
             {
-               if (spellString.equalsIgnoreCase("ALL"))
+               area.add(Integer.parseInt(a));
+            }
+            if (area.size() < 6)
+            {
+               for (int i = 0; i < 6; i++)
                {
-                  allDisallowed = true;
-               }
-               else
-               {
-                  disallowedSpells.add(Ollivanders2API.getSpells(this).getSpellTypeByName(spellString));
+                  area.set(i, 0);
                }
             }
-            if (type.equalsIgnoreCase("World"))
+            if (loc.getWorld() != null && loc.getWorld().getName().equals(world))
             {
-               if (loc.getWorld() != null && loc.getWorld().getName().equals(world))
+               if ((area.get(0) < x) && (x < area.get(3)))
                {
-                  if (allowedSpells.contains(spell) || allAllowed)
+                  if ((area.get(1) < y) && (y < area.get(4)))
                   {
-                     return true;
-                  }
-                  if (disallowedSpells.contains(spell) || allDisallowed)
-                  {
-                     cast = false;
-                  }
-               }
-            }
-            if (type.equalsIgnoreCase("Cuboid"))
-            {
-               String[] areaStringList = areaString.split(" ");
-               List<Integer> area = new ArrayList<>();
-               for (String a : areaStringList)
-               {
-                  area.add(Integer.parseInt(a));
-               }
-               if (area.size() < 6)
-               {
-                  for (int i = 0; i < 6; i++)
-                  {
-                     area.set(i, 0);
-                  }
-               }
-               if (loc.getWorld() != null && loc.getWorld().getName().equals(world))
-               {
-                  if ((area.get(0) < x) && (x < area.get(3)))
-                  {
-                     if ((area.get(1) < y) && (y < area.get(4)))
+                     if ((area.get(2) < z) && (z < area.get(5)))
                      {
-                        if ((area.get(2) < z) && (z < area.get(5)))
+                        if (allowedSpells.contains(spell) || allAllowed)
                         {
-                           if (allowedSpells.contains(spell) || allAllowed)
-                           {
-                              return true;
-                           }
-                           if (disallowedSpells.contains(spell) || allDisallowed)
-                           {
-                              cast = false;
-                           }
+                           return true;
+                        }
+                        if (disallowedSpells.contains(spell) || allDisallowed)
+                        {
+                           cast = false;
                         }
                      }
                   }
@@ -1925,6 +1928,7 @@ public class Ollivanders2 extends JavaPlugin
             }
          }
       }
+
       return cast;
    }
 
