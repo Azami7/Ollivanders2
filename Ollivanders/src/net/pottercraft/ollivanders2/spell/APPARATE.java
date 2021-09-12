392b1d54a4fd22a3aebba05bd1f1e1bd0d80546c
Mac <eurphus12@gmail.com>
2021-09-12 16:53:45 -0400

    - Removed Apparate changes

--- Ollivanders/src/net/pottercraft/ollivanders2/spell/APPARATE.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/spell/APPARATE.java
@@ -301,32 +301,6 @@ public final class APPARATE extends O2Spell
             common.printDebugMessage("Nullum Apparebit at destination blocks teleport.", null, null, false);
             return false;
         }
-
-        // check if x or z coordinates exceed max set in configuration
-        if ((destination.getX() >= p.getConfig().getInt("maxApparateDistance")) || (destination.getZ() >= p.getConfig().getInt("maxApparateDistance")))
-        {
-            common.printDebugMessage("Player is unable to teleport that far", null, null, false);
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Check to see if destination exceeds the maximum distance set in configuration
-     *
-     * @param destination the target location to apparate to
-     * @return true if the player can apparate, false otherwise
-     */
-    private boolean exceedsMaxDistance(@NotNull Location destination)
-    {
-        // check if x or z coordinates exceed max set in configuration
-        if ((destination.getX() >= p.getConfig().getInt("maxApparateDistance")) || (destination.getZ() >= p.getConfig().getInt("maxApparateDistance")))
-        {
-            common.printDebugMessage("Player is unable to teleport that far", null, null, false);
-            return false;
-        }
-
         return true;
     }
 
