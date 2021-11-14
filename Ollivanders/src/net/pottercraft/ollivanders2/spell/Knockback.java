9a849bdee2f187d4af55c3224a9efda662d1dbf3
Azami7 <kamundse@gmail.com>
2021-11-14 13:41:57 -0800

    Spell performance issues - added config to set faster spell casting since it was causing performance issues on at least one server - changed knockback WG checks to be more precise - renamed friendly animals list to friendly mobs since some are not Animals - fixed flipendo text to be more accurate

--- Ollivanders/src/net/pottercraft/ollivanders2/spell/Knockback.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/spell/Knockback.java
@@ -1,9 +1,13 @@
 package net.pottercraft.ollivanders2.spell;
 
+import com.sk89q.worldguard.protection.flags.Flags;
 import net.pottercraft.ollivanders2.Ollivanders2;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -20,7 +24,7 @@ public abstract class Knockback extends O2Spell
    boolean pull = false;
 
    /**
-    * This reduces strong the knockback is. 1 is max strength, any number higher than one will reduce the strength.
+    * This reduces how strong the knockback is. 1 is max strength, any number higher than one will reduce the strength.
     */
    protected int strengthReducer = 20;
 
@@ -72,32 +76,21 @@ public abstract class Knockback extends O2Spell
          // look for entities within radius of the projectile and knockback one of them
          for (Entity entity : entities)
          {
-            // first check entity whitelist
-            if (entityWhitelist.size() > 0 && !entityWhitelist.contains(entity.getType()))
+            // check to see if we can target this entity
+            if (!entityCheck(entity))
                continue;
-
-            // do not knockback the caster or any blacklisted entity
-            if (entity.getUniqueId() == player.getUniqueId() || entityBlacklist.contains(entity.getType()))
-            {
-               continue;
-            }
 
             double velocity = usesModifier / strengthReducer;
             if (velocity < minVelocity)
-            {
                velocity = minVelocity;
-            }
             else if (velocity > maxVelocity)
-            {
                velocity = maxVelocity;
-            }
 
             if (pull)
-            {
                velocity = velocity * -1;
-            }
 
             entity.setVelocity(player.getLocation().getDirection().normalize().multiply(velocity));
+            player.sendMessage("Successfully targeted " + entity.getName());
 
             kill();
             return;
@@ -107,5 +100,44 @@ public abstract class Knockback extends O2Spell
       // projectile is stopped, kill spell
       if (hasHitTarget())
          kill();
+   }
+
+   /**
+    * Determine if this entity can be targeted
+    *
+    * @param entity the entity to check
+    * @return true if it can be targeted, false otherwise
+    */
+   private boolean entityCheck (Entity entity)
+   {
+      // first check entity whitelist
+      if (entityWhitelist.size() > 0 && !entityWhitelist.contains(entity.getType()))
+         return false;
+
+      // do not knockback the caster or any blacklisted entity
+      if (entity.getUniqueId() == player.getUniqueId() || entityBlacklist.contains(entity.getType()))
+         return false;
+
+      //
+      // worldguard
+      //
+
+      // players
+      if (entity instanceof Player && !Ollivanders2.worldGuardO2.checkWGFlag(player, location, Flags.PVP))
+         return false;
+
+      // friendly mobs
+      if (entity instanceof Animals && !Ollivanders2.worldGuardO2.checkWGFlag(player, location, Flags.DAMAGE_ANIMALS))
+         return false;
+
+      // items
+      if (entity instanceof Item && !Ollivanders2.worldGuardO2.checkWGFlag(player, location, Flags.ITEM_PICKUP))
+         return false;
+
+      // vehicles
+      if (entity instanceof Vehicle && !Ollivanders2.worldGuardO2.checkWGFlag(player, location, Flags.RIDE))
+         return false;
+
+      return true;
    }
 }
