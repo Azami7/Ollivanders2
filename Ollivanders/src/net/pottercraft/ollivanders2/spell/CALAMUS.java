7d585e1d202ced65a46d265e200bbb3073464d22
Azami7 <kamundse@gmail.com>
2025-06-05 19:25:50 -0700

    Switching Spell
    - added switching spell permurate
    - fixed minor spell termination bug in TransfigurationBase
    - code reformat
    
    https://github.com/Azami7/Ollivanders2/issues/542

--- Ollivanders/src/net/pottercraft/ollivanders2/spell/CALAMUS.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/spell/CALAMUS.java
@@ -11,15 +11,13 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Turns sticks in to arrows.
  */
-public final class CALAMUS extends ItemToItemTransfiguration
-{
+public final class CALAMUS extends ItemToItemTransfiguration {
     /**
      * Default constructor for use in generating spell text.  Do not use to cast the spell.
      *
      * @param plugin the Ollivanders2 plugin
      */
-    public CALAMUS(Ollivanders2 plugin)
-    {
+    public CALAMUS(Ollivanders2 plugin) {
         super(plugin);
 
         spellType = O2SpellType.CALAMUS;
@@ -35,8 +33,7 @@ public final class CALAMUS extends ItemToItemTransfiguration
      * @param player    the player who cast this spell
      * @param rightWand which wand the player was using
      */
-    public CALAMUS(@NotNull Ollivanders2 plugin, @NotNull Player player, @NotNull Double rightWand)
-    {
+    public CALAMUS(@NotNull Ollivanders2 plugin, @NotNull Player player, @NotNull Double rightWand) {
         super(plugin, player, rightWand);
         spellType = O2SpellType.CALAMUS;
         branch = O2MagicBranch.TRANSFIGURATION;
--- Ollivanders/src/net/pottercraft/ollivanders2/spell/ItemToItemTransfiguration.java
+++ Ollivanders/src/net/pottercraft/ollivanders2/spell/ItemToItemTransfiguration.java
@@ -20,8 +20,7 @@ import java.util.Map;
 /**
  * Super class for all item entity transfigurations
  */
-public abstract class ItemToItemTransfiguration extends EntityTransfiguration
-{
+public abstract class ItemToItemTransfiguration extends EntityTransfiguration {
     /**
      * The list of changed items for non-permanent spells to revert
      */
@@ -42,8 +41,7 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      *
      * @param plugin the Ollivanders2 plugin
      */
-    public ItemToItemTransfiguration(Ollivanders2 plugin)
-    {
+    public ItemToItemTransfiguration(Ollivanders2 plugin) {
         super(plugin);
 
         branch = O2MagicBranch.TRANSFIGURATION;
@@ -56,8 +54,7 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      * @param player    the player who cast this spell
      * @param rightWand which wand the player was using
      */
-    public ItemToItemTransfiguration(@NotNull Ollivanders2 plugin, @NotNull Player player, @NotNull Double rightWand)
-    {
+    public ItemToItemTransfiguration(@NotNull Ollivanders2 plugin, @NotNull Player player, @NotNull Double rightWand) {
         super(plugin, player, rightWand);
 
         entityAllowedList.add(EntityType.ITEM);
@@ -67,42 +64,56 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      * Look for items and change them to the new type
      */
     @Override
-    void transfigure()
-    {
+    void transfigure() {
         if (isTransfigured)
             // we've already transfigured something
             return;
 
-        for (Entity entity : getCloseEntities(1.5))
-        {
-            p.getLogger().info("checking " + entity.getName());
+        for (Entity entity : getCloseEntities(1.5)) {
+            common.printDebugMessage("checking " + entity.getName(), null, null, false);
 
-            if (!canTransfigure(entity))
-            {
-                p.getLogger().info("!canTransfigure(entity)" + entity.getName());
-                continue;
-            }
-
-            if (!(entity instanceof Item))
-            {
-                p.getLogger().info("!(entity instanceof Item)" + entity.getName());
+            if (!(entity instanceof Item)) {
+                common.printDebugMessage("!(entity instanceof Item)" + entity.getName(), null, null, false);
                 continue;
             }
 
             Item item = (Item) entity;
             Material originalType = item.getItemStack().getType();
 
-            if (!transfigurationMap.containsKey(originalType))
-            {
-                p.getLogger().info("!transfigurationMap.containsKey(originalType)" + entity.getName());
+            if (!transfigurationMap.containsKey(originalType)) {
+                common.printDebugMessage("!transfigurationMap.containsKey(originalType)" + entity.getName(), null, null, false);
                 continue;
             }
 
             Material newType = transfigurationMap.get(originalType);
+            changeItem(item, newType);
+
+            if (isTransfigured)
+                sendSuccessMessage();
+            else
+                sendFailureMessage();
+
+            kill();
+            return;
+        }
+    }
+
+    /**
+     * Change an item to a new type
+     *
+     * @param item    the item to change
+     * @param newType the new item type to change to
+     */
+    void changeItem(Item item, Material newType) {
+        if (canTransfigure(item)) {
+
+            Material originalType = item.getItemStack().getType();
+
+            if (!permanent)
+                changedItems.put(item, originalType);
 
             int rand = Math.abs(Ollivanders2Common.random.nextInt() % 100);
-            if (rand < successRate)
-            {
+            if (rand < successRate) {
                 if (!permanent)
                     changedItems.put(item, originalType);
 
@@ -111,22 +122,18 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
                 item.setItemStack(itemStack);
 
                 isTransfigured = true;
-
-                player.sendMessage(Ollivanders2.chatColor + successMessage);
+                return;
             }
-            else
-                player.sendMessage(Ollivanders2.chatColor + failureMessage);
-
-            kill();
-            return;
         }
+
+        isTransfigured = false;
     }
 
     /**
      * Determine if this entity be transfigured by this spell.
      * <p>
      * Entity can transfigure if:
-     * 1. It is not in the bloked list
+     * 1. It is not in the blocked list
      * 2. It is in the allowed list, if the allowed list exists
      * 3. The entity is not already the target type
      * 4. There are no WorldGuard permissions preventing the caster from altering this entity type
@@ -136,19 +143,15 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      * @return true if it can be changed
      */
     @Override
-    protected boolean canTransfigure(@NotNull Entity entity)
-    {
+    protected boolean canTransfigure(@NotNull Entity entity) {
         boolean canTransfigure = super.canTransfigure(entity);
 
         // if all prev checks passed, now verify this item is not enchanted
-        if (canTransfigure)
-        {
-            if (Ollivanders2API.getItems().enchantedItems.isEnchanted((Item) entity))
-            {
+        if (canTransfigure) {
+            if (Ollivanders2API.getItems().enchantedItems.isEnchanted((Item) entity)) {
                 if (!canTransfigureEnchantedItems)
                     canTransfigure = false;
-                else
-                {
+                else {
                     Enchantment enchantment = Ollivanders2API.getItems().enchantedItems.getEnchantment(((Item) entity).getItemStack());
                     if (enchantment != null && enchantment.getType().getLevel().ordinal() > this.spellType.getLevel().ordinal())
                         canTransfigure = false;
@@ -163,13 +166,11 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      * Revert all items to their original types.
      */
     @Override
-    void doRevert()
-    {
+    void doRevert() {
         if (permanent)
             return;
 
-        for (Item item : changedItems.keySet())
-        {
+        for (Item item : changedItems.keySet()) {
             ItemStack itemStack = item.getItemStack();
             itemStack.setType(changedItems.get(item));
             item.setItemStack(itemStack);
@@ -183,8 +184,7 @@ public abstract class ItemToItemTransfiguration extends EntityTransfiguration
      * @return the transfigured entity
      */
     @Nullable
-    protected Entity transfigureEntity(@NotNull Entity entity)
-    {
+    protected Entity transfigureEntity(@NotNull Entity entity) {
         return entity;
     }
 }
